9461bc20bdec8e3290bb97d2568028fa0abe63e5
Projkeen <Projkeen@outlook.com>
2022-10-28 12:00:02 +0300

    next

--- TrainManagement.Web/Program.cs
+++ TrainManagement.Web/Program.cs
@@ -34,7 +34,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
-    pattern: "{controller=User}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=User}/{action=Login}/{id?}");
 
 app.Run();
